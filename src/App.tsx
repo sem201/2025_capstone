401b39b883d76f79e3e37f8defe658c16d3aceea
sem201 <sem201@naver.com>
2025-05-16 02:04:04 +0900

    Remove : 라우터 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { router } from "./router";
 import "./App.css";
-import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@styles/theme";
 import MainPage from "@pages/MainPage";
@@ -8,7 +6,6 @@ import MainPage from "@pages/MainPage";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      {/* <RouterProvider router={router}/> */}
       <MainPage />
     </ThemeProvider>
   );
